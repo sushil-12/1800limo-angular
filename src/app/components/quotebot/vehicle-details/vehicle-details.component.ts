4993325c6b52bcdc37cf9c393500e855b4351cb5
IshpreetSingh_10 <ishpreet.singh@infostride.com>
2022-11-07 21:01:28 +0530

    redesigned the vehicle details page and corrected dropdown from home component

--- src/app/components/quotebot/vehicle-details/vehicle-details.component.ts
+++ src/app/components/quotebot/vehicle-details/vehicle-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ErrorDialogService } from 'src/app/services/error-dialog/errordialog.service';
 import { MapsAPILoader } from '@agm/core'
+import * as moment from 'moment';
 
 declare let $: any
 
@@ -99,35 +100,9 @@ export class VehicleDetailsComponent implements OnInit
 					nav: false,
 					autoplay: false,
 					dots: false,
-					loop: true,
+					loop: false,
 					responsiveRefreshRate: 200,
 					responsiveClass: true,
-					navText: ['<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 66.91 122.88" style="enable-background:new 0 0 66.91 122.88" xml:space="preserve"><g><path d="M64.96,111.2c2.65,2.73,2.59,7.08-0.13,9.73c-2.73,2.65-7.08,2.59-9.73-0.14L1.97,66.01l4.93-4.8l-4.95,4.8 c-2.65-2.74-2.59-7.1,0.15-9.76c0.08-0.08,0.16-0.15,0.24-0.22L55.1,2.09c2.65-2.73,7-2.79,9.73-0.14 c2.73,2.65,2.78,7.01,0.13,9.73L16.5,61.23L64.96,111.2L64.96,111.2L64.96,111.2z"/></g></svg>',
-						'<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 66.91 122.88" style="enable-background:new 0 0 66.91 122.88" xml:space="preserve">  <g><path d="M1.95,111.2c-2.65,2.72-2.59,7.08,0.14,9.73c2.72,2.65,7.08,2.59,9.73-0.14L64.94,66l-4.93-4.79l4.95,4.8 c2.65-2.74,2.59-7.11-0.15-9.76c-0.08-0.08-0.16-0.15-0.24-0.22L11.81,2.09c-2.65-2.73-7-2.79-9.73-0.14 C-0.64,4.6-0.7,8.95,1.95,11.68l48.46,49.55L1.95,111.2L1.95,111.2L1.95,111.2z"/></g></svg> '],
-					responsive: {
-						0: {
-							items: 1,
-							nav: false,
-							loop: true,
-							slideSpeed: 10,
-							autoplay: true,
-							dots: false
-						},
-						600: {
-							items: 1,
-							nav: false,
-							loop: true,
-							slideSpeed: 10,
-							autoplay: false,
-							dots: false
-						},
-						1199: {
-							items: 1,
-							nav: false,
-							loop: true,
-							dots: false
-						}
-					}
 				})
 				.on("changed.owl.carousel", syncPosition);
 
@@ -394,6 +369,20 @@ export class VehicleDetailsComponent implements OnInit
 		localStorage.setItem('quotebot_form', JSON.stringify(obj))
 	}
 
+	/**
+	 * 
+	 * @param value :String [Required] date value
+	 * @returns String of format('LL') i.e. November 7, 2022
+	 */
+	formatDate(value: string): string
+	{
+		return moment(value).format('LL')
+	}
+
+	formatTime(value: string): string
+	{
+		return moment(value, 'HH:mm:ss').format('hh:mm a')
+	}
 
 
 
