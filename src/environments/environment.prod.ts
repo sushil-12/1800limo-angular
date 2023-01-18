c8dcdd91093e94f894dc54130fdeb48088c27632
IshpreetSingh_10 <ishpreet.singh@infostride.com>
2023-01-18 17:05:51 +0530

    env file - url changed for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
 	production: true,
-	// serverUrl:"https://api.1800limo.co/api/"
-	serverUrl: "https://api.1800limo.co/api/"
+	// serverUrl: "https://api.1800limo.co/api/"
+	serverUrl: "https://1800limoapi.infodevbox.com/api/"
 };
