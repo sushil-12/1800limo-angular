c073cf32c02470090157ae7a896f848a149c2249
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-03-17 17:38:54 +0530

    bug fixing

--- src/app/components/admin/add-individual-account/add-individual-account.component.ts
+++ src/app/components/admin/add-individual-account/add-individual-account.component.ts
@@ -112,7 +112,7 @@ export class AddIndividualAccountComponent implements OnInit
 			work: [''],
 			workIsd: ['+1', Validators.required],
 			workCountry: ['us'],
-			email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+			email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$/i)]],
 			address: ['', Validators.required],
 			city: ['', Validators.required],
 			state: ['', Validators.required],
--- src/app/components/admin/edit-individual-account/edit-individual-account.component.ts
+++ src/app/components/admin/edit-individual-account/edit-individual-account.component.ts
@@ -153,7 +153,7 @@ export class EditIndividualAccountComponent implements OnInit
 			work: [''],
 			workIsd: ['+1', Validators.required],
 			workCountry: ['us'],
-			email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+			email: ['', [Validators.required ,Validators.pattern(/^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$/i)]],
 			address: ['', Validators.required],
 			city: ['', Validators.required],
 			state: ['', Validators.required],
