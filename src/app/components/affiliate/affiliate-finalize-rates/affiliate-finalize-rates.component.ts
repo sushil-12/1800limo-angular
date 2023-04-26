0e186a65df63f8e0045f40c6323468eb169e4d9d
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-04-26 14:03:24 +0530

    Number of hours -- editable on finalize screen

--- src/app/components/affiliate/affiliate-finalize-rates/affiliate-finalize-rates.component.ts
+++ src/app/components/affiliate/affiliate-finalize-rates/affiliate-finalize-rates.component.ts
@@ -285,7 +285,22 @@ export class AffiliateFinalizeRatesComponent implements OnInit {
 			this.ReturnRatesForm.updateValueAndValidity();
 		}
 	}
+	changeInHours(hours:any){
+		console.log('in function change in hours-->>>',hours)
+		this.hours = Number(hours)
+			if (this.RateForm.all_inclusive_rates.controls.Base_Rate) {
+				this.hours > 0 && this.RatesForm && this.calculateAmount('RatesForm', 'all_inclusive_rates', 'Base_Rate');
+			}
+			else {
+				if (this.RateForm.all_inclusive_rates.controls.Milage_Rate) {
+					this.hours > 0 && this.RatesForm && this.calculateAmount('RatesForm', 'all_inclusive_rates', 'Milage_Rate');
+				}
+				if (this.RateForm.all_inclusive_rates.controls.Kilometer_Rate) {
+					this.hours > 0 && this.RatesForm && this.calculateAmount('RatesForm', 'all_inclusive_rates', 'Kilometer_Rate');
+				}
 
+			}
+	}
 	fetchRates(affiliate: string, bookingId: number = 0) {
 		this.$spinner.show()
 		this.affiliateService.getBookingData(bookingId).subscribe((response: any) => {
