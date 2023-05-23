e58684771626adfbda77012c9e828c120257a8e7
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-05-23 15:44:18 +0530

    changed text color pickup to green and dropoff to red on all preview screen

---
+++ src/app/components/admin/staff-permissions/staff-permissions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any
+@Component({
+  selector: 'app-staff-permissions',
+  templateUrl: './staff-permissions.component.html',
+  styleUrls: ['./staff-permissions.component.scss']
+})
+export class StaffPermissionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
