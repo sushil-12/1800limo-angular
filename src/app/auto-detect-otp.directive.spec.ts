64f131b908c40ab92836c98dc75ff56eef882c51
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-06-09 14:07:55 +0530

    added directive experiment-1

---
+++ src/app/auto-detect-otp.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AutoDetectOtpDirective } from './auto-detect-otp.directive';
+
+describe('AutoDetectOtpDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AutoDetectOtpDirective();
+    expect(directive).toBeTruthy();
+  });
+});
