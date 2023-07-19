e07aab955c506720287d10c4e95b5a828ddf57c9
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-07-19 16:36:07 +0530

    input field design fixes

--- src/app/components/admin/daily-bookings/daily-bookings.component.ts
+++ src/app/components/admin/daily-bookings/daily-bookings.component.ts
@@ -99,7 +99,7 @@ export class DailyBookingsComponent implements OnInit {
 		this.searchText = this.adminService.checkCookie('search') ?
 			this.adminService.getCookie('search')
 			: "";
-		
+		console.log('usedatefilter---->>>>>>>' , this.adminService.getCookie('useDateFilter'))
 		this.useDateFilter = this.adminService.checkCookie('useDateFilter') ?
 		(this.adminService.getCookie('useDateFilter')=='true' ? true : false)
 		: true;
