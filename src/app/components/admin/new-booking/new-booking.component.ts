37cbb4dce024471d0884e57ab4effaa28b6c0ad2
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-07-27 20:06:17 +0530

    comment

--- src/app/components/admin/new-booking/new-booking.component.ts
+++ src/app/components/admin/new-booking/new-booking.component.ts
@@ -512,9 +512,9 @@ export class NewBookingComponent implements OnInit {
 				service_type: response.data.service_type == 'oneway' ? 'one_way' : response.data['service_type'] == 'roundtrip' ? 'round_trip' : 'charter_tour',
 			})
 
-			// if (this.Form.updateType.value == 'edit') {
-			// 	this.booking_params.client_account_types.pop()
-			// }
+			if (this.Form.updateType.value == 'edit') {
+				this.booking_params.client_account_types.pop()
+			}
 			this.booking_id = this.Form.reservation_id.value;
 			this.Form.affiliate_id.value != 0 ? this.chooseAffiliate() : ''
 			try {
