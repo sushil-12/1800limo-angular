cac61563fdd2553c130749926e912626a7286967
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-08-04 13:49:48 +0530

    removed auto verfied opt

--- src/app/components/website/otp/otp.component.ts
+++ src/app/components/website/otp/otp.component.ts
@@ -115,7 +115,7 @@ export class OtpComponent implements OnInit, OnDestroy {
 					this.otpForm.get('otp').setValue(params.otp)
 					this.otpForm.updateValueAndValidity()
 					// this.ngOtpInput.focusTo(this.otpInput)
-					this.otpCheck()
+					// this.otpCheck()
 				}, 2000)
 			}
 		})
@@ -152,9 +152,9 @@ export class OtpComponent implements OnInit, OnDestroy {
 	onOtpChange(value) {
 		this.otpForm.get('otp').setValue(value)
 		this.otpForm.updateValueAndValidity()
-		if(value.length >=6){
-			this.otpCheck()
-		}
+		// if(value.length >=6){
+		// 	this.otpCheck()
+		// }
 	}
 
 	resendOtp() {
