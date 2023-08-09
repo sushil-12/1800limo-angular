2b0c53b6d64dfcc7aa0ee6f484a85fcd10f9deab
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-08-09 15:44:40 +0530

    farm-out driver communication, added drop-down item , added cruze port departure rate bucket

--- src/app/components/affiliate/farm-out/farm-out.component.ts
+++ src/app/components/affiliate/farm-out/farm-out.component.ts
@@ -432,6 +432,13 @@ export class FarmOutComponent implements OnInit
 				? this.passengerDetails.pax_tel
 				: this.passengerDetails.passenger_email;
 			this.reciptentName = this.passengerDetails.passenger_name;
+		} else if (this.passengerDetails.selection_button == "Driver") {
+			console.log('driver')
+			this.sendInformation = format
+				? this.passengerDetails?.driver_cell_isd +
+				this.passengerDetails?.driver_cell_number
+				: this.passengerDetails?.driver_email;
+			this.reciptentName = this.passengerDetails?.driver_name;
 		} else {
 			this.sendInformation = format
 				? this.passengerDetails.loose_affiliate_phone_isd +
