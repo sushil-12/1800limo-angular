b2db848d1655c28479e206c4733ea1f66a3e01e0
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-08-29 12:23:31 +0000

    removed auto verfication otp

--- src/app/components/website/otp/otp.component.ts
+++ src/app/components/website/otp/otp.component.ts
@@ -152,9 +152,9 @@ export class OtpComponent implements OnInit, OnDestroy {
 	onOtpChange(value) {
 		this.otpForm.get('otp').setValue(value)
 		this.otpForm.updateValueAndValidity()
-		if(value.length >=6){
-			this.otpCheck()
-		}
+		// if(value.length >=6){
+		// 	this.otpCheck()
+		// }
 	}
 
 	resendOtp() {
