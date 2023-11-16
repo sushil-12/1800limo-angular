dbf4f0044d6983f9b41fab25251a54b2019ebe38
Tushar Bansal <btushar700@gmail.com>
2023-11-16 14:52:28 +0530

    warning ui fixed all areas, eamil error fixed

--- src/app/components/affiliate/add-driver-from-affiliate/add-driver-from-affiliate.component.ts
+++ src/app/components/affiliate/add-driver-from-affiliate/add-driver-from-affiliate.component.ts
@@ -177,7 +177,7 @@ export class AddDriverFromAffiliateComponent
 			CellNumberCountry: ["us", Validators.required],
 			Email: [
 				"",
-				[Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")],
+				[Validators.pattern("^[a-zA-Z0-9.]+@[a-z0-9.-]+\\.[a-z]{2,4}$")],
 			],
 			Dress: [1, Validators.required],
 			StartDate: ["", [Validators.required]],
