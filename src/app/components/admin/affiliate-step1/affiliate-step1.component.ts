62737680fb8c2b4a1b3f0fb68575858481182e52
Sukhvir Singh <sukhvir.singh@infostride.com>
2023-11-23 21:59:57 +0530

    fix scroll

--- src/app/components/admin/affiliate-step1/affiliate-step1.component.ts
+++ src/app/components/admin/affiliate-step1/affiliate-step1.component.ts
@@ -123,6 +123,10 @@ export class AffiliateStep1Component implements OnInit {
 			{
 				this.startBusinessYears = data;
 			});
+			window.scrollTo({
+				top :0,
+				behavior:'smooth'
+			})
 		this.adminService.getAssicationsLanguages()
 			.pipe(
 				catchError(err => {
@@ -278,10 +282,7 @@ export class AffiliateStep1Component implements OnInit {
 							console.log('associations');
 							//
 							// this.stateManagementService.setprogressBar(false);
-							window.scrollTo({
-								top :0,
-								behavior:'smooth'
-							})
+						
 						});
 				} else {
 					this.addAffiliateAccountForm.patchValue({
@@ -290,10 +291,7 @@ export class AffiliateStep1Component implements OnInit {
 					// this.stateManagementService.setprogressBar(false);
 
 					this.onLanguageChange('1', true);//set english as default language
-					window.scrollTo({
-						top :0,
-						behavior:'smooth'
-					})
+				
 				}
 
 		this.spinner.hide()
