9c798101f4709a9b91bc863b2c44ab50d696800e
Tushar Bansal <btushar700@gmail.com>
2024-01-18 17:58:05 +0530

    create bookind individual

--- src/app/services/individual.service.ts
+++ src/app/services/individual.service.ts
@@ -16,11 +16,11 @@ export class IndividualService {
     private authService: AuthService
   ) { }
 
-  createIndividualAccount(data,is_profile_complete) {
-    if(is_profile_complete){
+  createIndividualAccount(data, is_profile_complete) {
+    if (is_profile_complete) {
       return this.httpClient.post(this.serverUrl + 'edit-individual-account', data);
     }
-    else{
+    else {
       return this.httpClient.post(this.serverUrl + 'add-individual-account', data);
     }
   }
@@ -28,13 +28,29 @@ export class IndividualService {
   getIndividualAccountDetails() {
     return this.httpClient.get(this.serverUrl + `get-profile-data`);
   }
-  
+
   addIndividualCreditCard(data) {
-    return this.httpClient.post(this.serverUrl + `add-credit-card`,data);
+    return this.httpClient.post(this.serverUrl + `add-credit-card`, data);
   }
 
   deleteIndividualCreditCard(card_id) {
     return this.httpClient.delete(this.serverUrl + `delete-credit-card/${card_id}`);
-}
+  }
+
+  createBooking(data: any, update_type: string) {
+    if (update_type == 'return' || update_type == 'repeat') {
+      return this.httpClient.post(`${this.serverUrl}duplicate-reservation`, data)
+    }
+    if (data.reservation_id) {
+      return this.httpClient.put(this.serverUrl + 'edit-reservation', data);
+    }
+    else {
+      return this.httpClient.post(this.serverUrl + 'create-reservation', data);
+    }
+  }
+
+  getAccountDetails() {
+    return this.httpClient.get(this.serverUrl + `get-account-details`);
+  }
 
 }
