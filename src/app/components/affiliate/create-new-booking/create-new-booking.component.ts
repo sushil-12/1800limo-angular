192d1039a345c19fb5eaf816fe44449fd74842c9
tusharbansal1 <tushar.bansal@infostride.com>
2024-08-12 15:05:37 +0530

    25% of extra tip to admin share

--- src/app/components/affiliate/create-new-booking/create-new-booking.component.ts
+++ src/app/components/affiliate/create-new-booking/create-new-booking.component.ts
@@ -1540,6 +1540,7 @@ export class CreateNewBookingComponent implements OnInit {
 			let grandTotal = this.BookingForm.value.rateArray.grand_total
 			let stripeFee = grandTotal * 0.05 + 0.30
 			let adminShare = (base_rate * this.adminSharePercent) / 100
+			adminShare = adminShare + (this.BookingForm.value.rateArray.misc.Extra_Gratuity.amount * 0.25)
 			let deducted_admin_share = adminShare - stripeFee
 			let shareArray = {
 				baseRate: base_rate,
@@ -1575,6 +1576,7 @@ export class CreateNewBookingComponent implements OnInit {
 			let returnGrandTotal = this.BookingForm.value.return_grand_total
 			let stripeFee = returnGrandTotal * 0.05 + 0.30
 			let adminShare = (base_rate * this.adminSharePercent) / 100
+			adminShare = adminShare + (this.BookingForm.value.returnRateArray.misc.Extra_Gratuity.amount * 0.25)
 			let returnShareArray = {
 				baseRate: base_rate,
 				returnGrandTotal: returnGrandTotal,
