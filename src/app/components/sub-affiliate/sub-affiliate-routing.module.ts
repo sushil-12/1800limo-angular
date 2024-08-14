dc38a47a290ff02c7c52751e3f99854412dea115
tusharbansal1 <tushar.bansal@infostride.com>
2024-08-14 12:46:58 +0530

    sub aff profile,

--- src/app/components/sub-affiliate/sub-affiliate-routing.module.ts
+++ src/app/components/sub-affiliate/sub-affiliate-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { SubAffiliateGuard } from '../../guards/sub-affiliate.guard'
 import { MyBookingsComponent } from '../affiliate/my-bookings/my-bookings.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CreateNewBookingComponent } from '../affiliate/create-new-booking/create-new-booking.component';
+import { NewBookingComponent } from '../quotebot/new-booking/new-booking.component';
+import { AffiliateFinalizeComponent } from '../affiliate/affiliate-finalize/affiliate-finalize.component';
+import { FarmOutComponent } from '../affiliate/farm-out/farm-out.component';
 
 
 const routes: Routes = [
@@ -17,9 +21,28 @@ const routes: Routes = [
     // canActivate: [SubTravelAgentGuardGuard],
   },
   {
-		path: 'my-bookings',
+		path: 'bookings',
 		canActivate: [SubAffiliateGuard],
 		component: MyBookingsComponent
+	},
+  {
+		path: 'farm-out',
+		component: FarmOutComponent
+	},
+  {
+		path: 'create-new-booking',
+		canActivate: [SubAffiliateGuard],
+		component: CreateNewBookingComponent
+	},
+	{
+		path: 'new-booking',
+		canActivate: [SubAffiliateGuard],
+		component: NewBookingComponent
+	},
+  {
+		path: 'finalize-booking',
+		canActivate: [SubAffiliateGuard],
+		component: AffiliateFinalizeComponent
 	},
 
 ];
