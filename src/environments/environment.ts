1465464af5452c3974961aaf45f3650c8ddd18eb
Tushar <tushar.bansal@infostride.com>
2025-03-11 16:55:11 +0530

    fixed serach for loose aff onSearchLooseAffiliateId

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
 	production: false,
 	environmentName: 'Development',
-	// serverUrl: "http://10.20.20.66:8000/api/",
+	// serverUrl: "http://10.20.20.63:8000/api/",
 	accessKeyId: "AKIAZTOUHGGRSIV2GIVY",
 	secretAccessKey: "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",  
 	// recaptchaKey:"6Lcf2MYqAAAAAMPwdv97BZ_LBwX-1e9y2NcFsoIn",
