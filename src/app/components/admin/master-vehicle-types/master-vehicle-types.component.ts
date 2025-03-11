6528217cfc17d6781ef8929228beb7c4234dfe82
Tushar <tushar.bansal@infostride.com>
2025-03-11 16:08:04 +0530

    added toggle for rates based on currency

--- src/app/components/admin/master-vehicle-types/master-vehicle-types.component.ts
+++ src/app/components/admin/master-vehicle-types/master-vehicle-types.component.ts
@@ -6,6 +6,7 @@ import { NgxSpinnerService } from "ngx-spinner";
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { ThemePalette } from '@angular/material/core';
 declare var $: any;
 
 
@@ -34,6 +35,9 @@ export class MasterVehicleTypesComponent implements OnInit {
 	public editVehiclePic: string;
 	vehicleId: any;
 	currentUser:any;
+	color: ThemePalette = 'primary';
+	disabled = false;
+	convertCurrenyResp:any;
 
 	constructor(
 		private adminService: AdminService,
@@ -78,6 +82,31 @@ export class MasterVehicleTypesComponent implements OnInit {
 			seats: [1],
 			luggage: [0]
 		});
+	}
+
+	enableDisableClicked(event) {
+		this.spinner.show();//show spinner
+		console.log(event.checked);
+		if (event.checked) {
+			var status = 1;
+		}
+		else {
+			var status = 0;
+		}
+		this.adminService.converCurrenyEnableDisable(status)
+			.pipe(
+				catchError(err => {
+					this.spinner.hide();//hide spinner
+					return throwError(err);
+				})
+			).subscribe((result:any) => {
+				this.spinner.hide(); // Hide spinner when successful
+				this.convertCurrenyResp = result.data.convert_currency == 0 ? false : true 
+				  this.currentUser.convert_currency = result.data.convert_currency;
+				  localStorage.setItem("currentUser", JSON.stringify(this.currentUser));
+	          
+			});
+				
 	}
 
 	drop(event: CdkDragDrop<string[]>) {
