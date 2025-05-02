504224c3852414f92e0156ea107d57b9c7497ca7
= <=>
2025-05-02 12:54:17 +0530

    updated testing environment

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,5 +1,7 @@
 export const environment = {
 	production: true,
 	environmentName: 'Testing',
-	serverUrl: "https://1800limoapiqa.infodevbox.com/api/"
+	serverUrl: "https://1800limoapiqa.infodevbox.com/api/",
+	accessKeyId: "AKIAZTOUHGGRSIV2GIVY",
+	secretAccessKey: "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",  
 };
