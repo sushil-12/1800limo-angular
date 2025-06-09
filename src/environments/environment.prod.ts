a7abd179eae092bb2fb9000354f96bd9df7656a0
= <=>
2025-06-09 17:21:14 +0530

    prod backend url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
 	production: true,
 	environmentName: 'Production',
-	serverUrl: "https://laravelapi.1800limo.com/api/",
+	serverUrl: "https://api.1800limo.com/api/",
 	accessKeyId: "AKIAZTOUHGGRSIV2GIVY",
 	secretAccessKey: "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",
 	// recaptchaKey:"6Lcf2MYqAAAAAMPwdv97BZ_LBwX-1e9y2NcFsoIn",
