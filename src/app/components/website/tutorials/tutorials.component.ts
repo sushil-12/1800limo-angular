deb58bd04dc09e1c040e250e6e17286cc75f117d
= <=>
2025-06-27 16:36:13 +0530

    tutorials updated

--- src/app/components/website/tutorials/tutorials.component.ts
+++ src/app/components/website/tutorials/tutorials.component.ts
@@ -13,7 +13,7 @@ declare var $: any;
 export class TutorialsComponent implements OnInit {
 
   public fleetContents: any;
-  groupedTutorials:any;
+  groupedTutorials: any;
 
   constructor(private adminServices: AdminService, private spinner: NgxSpinnerService) { }
 
@@ -39,115 +39,128 @@ export class TutorialsComponent implements OnInit {
       });
     });
 
-  
+
 
     this.fleetContents = [
       {
         "title": "How to use the search engine",
-        "category": "Create and Manage Bookings", 
+        "category": "Create and Manage Bookings",
         "image": "assets/images/images_tutorial/updated-images/searchrnigne.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+use+the+search+for+vehicles%2C+and+rates+in+the+search+engine%2C+Quote-bot+screen+and+filters).mp4',
         "content": "A guide on how to use the search for vehicles, rates, and filters in the search engine and Quote-bot screen.",
       },
       {
         "title": "How to edit a booking",
-        "category": "Create and Manage Bookings", 
+        "category": "Create and Manage Bookings",
         "image": "assets/images/images_tutorial/updated-images/editrepeatretrun.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+edit+a+booking+from+one-way+to+round+trip+or+charter+Tour+from+the+booking+dashboard..mp4',
         "content": "A guide on how to edit a booking from one-way to round trip or charter tour from the booking dashboard.",
       },
       {
         "title": "How to create a repeat booking",
-        "category": "Create and Manage Bookings", 
+        "category": "Create and Manage Bookings",
         "image": "assets/images/images_tutorial/updated-images/editrepeatretrun.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Repeat+a+booking.mp4',
         "content": "A guide on how to create a repeat booking.",
       },
       {
         "title": "How to create a return booking",
-        "category": "Create and Manage Bookings", 
+        "category": "Create and Manage Bookings",
         "image": "assets/images/images_tutorial/updated-images/editrepeatretrun.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Return+a+booking.mp4',
         "content": "A guide on how to create a return booking.",
       },
       {
         "title": "How to register as an affiliate Operator",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/loginasaffiliate.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+Register+Affiliate+and+step+0.mp4',
         "content": "A step-by-step guide on how to sign up and register for an affiliate program.",
       },
       {
         "title": "How to complete step 1 as an affiliate",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/step-1.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+complete+step+1+to+register+as+affiliate.mp4',
         "content": "A guide on how to complete step 1 for registering as an affiliate.",
       },
       {
         "title": "How to complete step 2 as an affiliate",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/step-2.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+complete+step+2+to+register+as+affiliate.mp4',
         "content": "A guide on how to complete step 2 for registering as an affiliate.",
       },
       {
         "title": "How to complete step 3 as an affiliate",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/step-3.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+complete+step+3+to+register+as+affiliate.mp4',
         "content": "A guide on how to complete step 3 for registering as an affiliate.",
       },
       {
         "title": "How to complete step 4 as an affiliate",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/step-4.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+complete+step+4+to+register+as+affiliate.mp4',
         "content": "A guide on how to complete step 4 for registering as an affiliate.",
       },
       {
         "title": "How to complete step 5 as an affiliate",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/step-5.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+complete+step+5+to+register+as+affiliate.mp4',
         "content": "A guide on how to complete step 5 for registering as an affiliate.",
       },
       {
         "title": "How to create a farmout booking",
-        "category": "Register as an Affiliate Operator", 
+        "category": "Register as an Affiliate Operator",
         "image": "assets/images/images_tutorial/updated-images/searchrnigne.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+Create+Farmout+booking.mp4',
         "content": "A guide on how to create a farmout booking and earn 10% commission.",
       },
       {
         "title": "How to register as a Travel Agent",
-        "category": "Register as a Travel Agent", 
+        "category": "Register as a Travel Agent",
         "image": "assets/images/images_tutorial/updated-images/registerasagent.png",
-        "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Travel+Agent+Registration+(3).mp4',
+        "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+register+as+Travel+Agent+-+Step+1+and+2.mp4',
         "content": "A step-by-step guide on how to sign up and register for a Travel Agent.",
       },
       {
         "title": "How to create a booking as a Travel Agent",
-        "category": "Register as a Travel Agent", 
+        "category": "Register as a Travel Agent",
         "image": "assets/images/images_tutorial/updated-images/searchrnigne.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+Create+Booking+on+Travel+Agent+Account.mp4',
         "content": "A guide on how to create a booking as an travel agent and earn 10% commission.",
       },
       {
         "title": "How to register as Individual/Customer",
-        "category": "Register as Individual/Customer", 
+        "category": "Register as Individual/Customer",
         "image": "assets/images/images_tutorial/updated-images/registerasindv.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Individual+Signup.mp4',
         "content": "A step-by-step guide on how to sign up and register for an Individual account.",
       },
       {
         "title": "How to add a family member in Individual account",
-        "category": "Register as Individual/Customer", 
+        "category": "Register as Individual/Customer",
         "image": "assets/images/images_tutorial/updated-images/familymember.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Adding+family+members+other+members+to+the+Individual+account.mp4',
         "content": "A guide on how to add a family member to your individual account.",
       },
-     
+      {
+        "title": "How to choose correct account to Register",
+        "category": "How to choose correct account",
+        "image": "assets/images/images_tutorial/updated-images/searchrnigne.png",
+        "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+choose+correct+account+to+register.mp4',
+        "content": "A guide on how to choose correct account to register on the platform.",
+      },
+      {
+        "title": "How to choose correct account to Register",
+        "category": "How to choose correct account",
+        "image": "assets/images/images_tutorial/updated-images/searchrnigne.png",
+        "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/How+to+choose+correct+account+to+register.mp4',
+        "content": "A guide on how to choose correct account to register on the platform.",
+      },
       {
         "title": "How to advertise you fleet for free",
         "category": "Buy Chauffer Select Software",
@@ -185,14 +198,14 @@ export class TutorialsComponent implements OnInit {
       },
       {
         "title": "How to do search on daily booking frame",
-        "category": "Tutorials for Admin / Susbcriber", 
+        "category": "Tutorials for Admin and Chauffer Select Subscribers",
         "image": "assets/images/images_tutorial/updated-images/searchondailybooking.png",
-        "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Daily+booking+frame+Search.mp4',
+        "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Search+bookings+on+daily+booking+frame.mp4',
         "content": "A guide on how to do search using booking number, cell phone, name etc. on daily booking frame.",
       },
       {
         "title": "How to add sub admin",
-        "category": "Tutorials for Admin / Susbcriber",
+        "category": "Tutorials for Admin and Chauffer Select Subscribers",
         "image": "assets/images/images_tutorial/updated-images/addsubadmin.png",
         "link": 'https://1800limo.s3.us-east-2.amazonaws.com/tutorials/Add+Sub+Admin.mp4',
         "content": "A guide on how to add a sub admin.",
