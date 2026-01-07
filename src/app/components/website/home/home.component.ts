eb364eb8b47844fdd82728a4c33ca5828b4565b3
Tushar <tushar.bansal@infostride.com>
2026-01-07 17:37:42 +0530

    added current location on homepage in pickup address and added acc_id in edit aff veh fare aff side

--- src/app/components/website/home/home.component.ts
+++ src/app/components/website/home/home.component.ts
@@ -15,6 +15,8 @@ import { constant_data } from '../../../../assets/js/data.js'
 import * as moment from 'moment';
 import { Swiper } from 'swiper';
 import { Navigation, Autoplay } from 'swiper/modules';
+import { HttpClient } from '@angular/common/http';
+
 
 declare var $: any;
 
@@ -107,6 +109,7 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
 		private quotebotService: QuotebotService,
 		private globalFunctions: SharedModule,
 		private elementRef: ElementRef,
+		private http: HttpClient
 
 	) { }
 
@@ -120,6 +123,7 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
 
 	ngOnInit() {
 
+		this.setCurrentLocationToPickup();
 
 		try {
 			const elementsWithTabIndex = document.querySelectorAll('[tabindex]');
@@ -361,6 +365,42 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
 			}, 200)
 		})
 	}
+
+	setCurrentLocationToPickup() {
+		if (!navigator.geolocation) {
+		  return;
+		}
+	  
+		navigator.geolocation.getCurrentPosition(
+		  (position) => {
+			const lat = position.coords.latitude;
+			const lng = position.coords.longitude;
+	  
+			this.reverseGeocodeAndFill(lat, lng);
+		  },
+		  () => {
+			// User denied location – do nothing
+		  },
+		  {
+			enableHighAccuracy: true,
+			timeout: 10000
+		  }
+		);
+	  }
+	  
+	  reverseGeocodeAndFill(lat: number, lng: number) {
+		// FREE OpenStreetMap reverse geocoding
+		const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`;
+	  
+		this.http.get<any>(url).subscribe(res => {
+		  if (res?.display_name) {
+			this.quoteBotForm.patchValue({
+			  pickup_address: res.display_name
+			});
+		  }
+		});
+	  }
+	  
 
 
 	fetchPageData(section: string) {
