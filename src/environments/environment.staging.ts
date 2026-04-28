1aaace9374f574ca7b9b6e774b5c6e983122e13f
Simran <simran.ghai@infostride.com>
2026-04-28 17:50:17 +0530

    Few changes related to environment files

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,8 +2,7 @@ export const environment = {
 	production: true,
 	environmentName: 'Staging',
 	serverUrl: "https://1800limoapi.infodevbox.com/api/",
-	accessKeyId: "AKIAZTOUHGGRSIV2GIVY",
 	mistralSecretKey: "WvrrM5g1bmpJZjb3Go4d8tnUfZkNogmP",
-	secretAccessKey: "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",  
+	encryptionKey:"BTEo5hJauptWuoKUqeTfVgEn1a6BOSXzNlSoN6SPQBI=", 
 	// recaptchaKey:"6Lcf2MYqAAAAAMPwdv97BZ_LBwX-1e9y2NcFsoIn",
 };
