62d6aa196501837f59866abe99e25d22c3a3817c
Sushil <sushil.kumar@infostride.com>
2026-05-06 14:06:15 +0530

    pushed to workdpress

--- src/app/components/website/blog-detail/blog-detail.component.ts
+++ src/app/components/website/blog-detail/blog-detail.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Title, Meta, DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { BlogPost, BlogComment } from '../blog/blog.data';
 import { BlogService } from '../blog/blog.service';
+
+const WP_BLOCK_CSS_ID = 'wp-block-library-common-css';
+const WP_BLOCK_CSS_HREF = 'https://blog.1800limo.com/wp-includes/css/dist/block-library/common.min.css';
 
 @Component({
     selector: 'app-blog-detail',
     templateUrl: './blog-detail.component.html',
     styleUrls: ['./blog-detail.component.scss']
 })
-export class BlogDetailComponent implements OnInit {
+export class BlogDetailComponent implements OnInit, OnDestroy {
 
     post: BlogPost | undefined;
     safeContent: SafeHtml | undefined;
@@ -33,6 +36,7 @@ export class BlogDetailComponent implements OnInit {
     ) { }
 
     ngOnInit(): void {
+        this.injectWpBlockStyles();
         this.route.paramMap.subscribe(params => {
             const slug = params.get('id');
             if (slug) {
@@ -41,6 +45,22 @@ export class BlogDetailComponent implements OnInit {
                 this.router.navigate(['/blog']);
             }
         });
+    }
+
+    ngOnDestroy(): void {
+        const link = document.getElementById(WP_BLOCK_CSS_ID);
+        if (link && link.parentNode) {
+            link.parentNode.removeChild(link);
+        }
+    }
+
+    private injectWpBlockStyles(): void {
+        if (document.getElementById(WP_BLOCK_CSS_ID)) return;
+        const link = document.createElement('link');
+        link.id = WP_BLOCK_CSS_ID;
+        link.rel = 'stylesheet';
+        link.href = WP_BLOCK_CSS_HREF;
+        document.head.appendChild(link);
     }
 
     loadPost(slug: string) {
